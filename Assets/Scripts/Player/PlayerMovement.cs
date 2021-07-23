39f4fb4e4faebd6fe0c3d2786d5e2f2165d7bed4
hisham-css <61122654+hisham-CSS@users.noreply.github.com>
2021-07-23 10:53:22 -0400

    Lab 3 unfinished

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer), typeof(Animator))]
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+    Animator anim;
+
+    public float speed;
+    public int jumpForce;
+
+    public bool isGrounded;
+    public LayerMask isGroundLayer;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+
+        if (speed <= 0)
+        {
+            speed = 5.0f;
+        }
+
+        if (jumpForce <= 0)
+        {
+            jumpForce = 300;
+        }
+
+        if (groundCheckRadius <= 0)
+        {
+            groundCheckRadius = 0.05f;
+        }
+
+        if (!groundCheck)
+        {
+            Debug.Log("Groundcheck not assigned, please assign a value to groundcheck");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, isGroundLayer);
+
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = Vector2.zero;
+            rb.AddForce(Vector2.up * jumpForce);
+        }
+
+        Vector2 moveDirection = new Vector2(horizontalInput * speed, rb.velocity.y);
+        rb.velocity = moveDirection;
+
+        anim.SetFloat("speed", Mathf.Abs(horizontalInput));
+        anim.SetBool("isGrounded", isGrounded);
+
+
+        if (sr.flipX && horizontalInput > 0 || !sr.flipX && horizontalInput < 0)
+            sr.flipX = !sr.flipX;
+
+  
+
+
+    }
+
+}
